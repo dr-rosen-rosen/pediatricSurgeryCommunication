b1625ede8cabdec9852d35d7605536dbcb4d18f2
Mike Rosen <mrosena@gmail.com>
2023-09-01 14:45:51 -0400

    Updated to reading in transcripts across studies

--- 0_main.R
+++ 0_main.R
@@ -10,4 +10,51 @@
 library(tidyverse)
 library(here)
 library(config)
+source('1_transciptingest.R')
 
+######################
+######################
+# read in transcripts:
+######################
+######################
+
+
+# for BIAS transcripts:
+bias_transcripts <- getBiasTranscripts(
+  dirs = c('/Volumes/LSM/Boss\ Peds\ Surgery\ Transcripts\ (LSM)/Bias\ Transcripts/Bias_Visits_with_1_clinician_surgeon',
+          '/Volumes/LSM/Boss\ Peds\ Surgery\ Transcripts\ (LSM)/Bias\ Transcripts/Bias\ Visits\ with\ 2\ transcripts\ each\ (clinician_supporting)')
+)
+table(bias_transcripts$speaker)
+write.csv(table(bias_transcripts$speaker),'bias_speaker.csv')
+write.csv(table(bias_transcripts$speaker,bias_transcripts$file_id),'bias_speakerByFile.csv')
+
+# for SDM transcripts:
+sdm_transcripts <- getSDMTranscripts(
+  dirs = c('/Volumes/LSM/Boss\ Peds\ Surgery\ Transcripts\ (LSM)/SDM\ Transcripts/SDM\ visits\ with\ one\ clinician',
+           '/Volumes/LSM/Boss\ Peds\ Surgery\ Transcripts\ (LSM)/SDM\ Transcripts/SDM\ visits\ with\ 2\ clinicians\ each')
+)
+table(sdm_transcripts$speaker)
+write.csv(table(sdm_transcripts$speaker),'SDM_speaker.csv')
+write.csv(table(sdm_transcripts$speaker,sdm_transcripts$file_id),'SDM_speakerByFile.csv')
+
+# for Connects transcripts:
+connects_transcripts <- getConnectsTranscripts(
+  dirs = c('/Volumes/LSM/Boss\ Peds\ Surgery\ Transcripts\ (LSM)/CONNECTS\ Transcripts/CONNECTS\ visits\ with\ 1\ clinician',
+           '/Volumes/LSM/Boss\ Peds\ Surgery\ Transcripts\ (LSM)/CONNECTS\ Transcripts/CONNECTS\ visits\ with\ 2\ clinicians')
+)
+table(connects_transcripts$speaker)
+write.csv(table(connects_transcripts$speaker),'connects_speaker.csv')
+write.csv(table(connects_transcripts$speaker,connects_transcripts$file_id),'connects_speakerByFile.csv')
+
+cmb_transcripts <- bind_rows(
+  bias_transcripts,
+  sdm_transcripts,
+  connects_transcripts
+) %>%
+  mutate(
+    speaker = case_match(speaker,
+                         c('dr', 'doctor','physician') ~ 'dr',
+                         .default = speaker)
+  )
+
+write.csv(table(cmb_transcripts$speaker),'cmbd_speakers.csv')
--- 1_transcriptIngest.R
+++ 1_transcriptIngest.R
@@ -11,115 +11,207 @@ library(officer)
 library(tidyverse)
 library(here)
 
-# Get a list of all of the file names
-dir <- 
-  # bias transcripts with one clinician
-  # '/Users/mrosen44/Johns Hopkins/Anne Links - Boss Peds Surgery Transcripts (LSM)/Bias Transcripts/Bias Visits with 1 clinician (surgeon)'
-  # SDM transcripts with one clinician
-  '/Users/mrosen44/Johns Hopkins/Anne Links - Boss Peds Surgery Transcripts (LSM)/SDM Transcripts/SDM visits with one clinician'
-fNames <- list.files(path = dir, pattern = '*.docx')
-
-# To test using just one file, do something like:
-# fNames <- "10002_Edited Deidentified.docx"  
-transcript_df <- data.frame(
-  file_id = character(),
-  Speaker = character(),
-  speech = character(),
-  text = character(),
-  text_ok = logical(),
-  text_pieces = integer(),
-  text_remainder = character()
-)
 
 ######################
 ######################
 # for BIAS transcripts:
 ######################
 ######################
-for (f in fNames) {
-  t <- docxtractr::read_docx(here(dir,f))
-  tbl_no <- docxtractr::docx_tbl_count(t)
-  print(paste0(f,'; number of tables: ',tbl_no))
-  if (tbl_no == 0) {
-    df <- officer::read_docx(here(dir,f)) %>%
-      officer::docx_summary() %>%
-      select(text) %>%
-      separate_wider_delim(text, ":",
-                           names = c("Speaker", "speech"),
-                           too_many = "merge",
-                           too_few = "debug")
-    df['file_id'] <- substr(f,1,5)
-    transcript_df <- rbind(transcript_df,df)
-  } else if (tbl_no == 1) {
-    df <- docxtractr::docx_extract_tbl(t, 1, header = FALSE) %>%
-      mutate(
-        V1 = stringr::str_remove(V1, pattern = " .*"),
-        V1 = tolower(V1),
-        file_id = substr(f,1,5)
-      ) %>%
-      rename('Speaker' = V1,'speech' = V2)
-    transcript_df <- bind_rows(transcript_df,df)
-  } else {
-    print(paste('Too many tables for:',f))
-  }
-}
 
-transcript_df <- transcript_df %>%
-  janitor::clean_names() %>%
-  mutate(
-    speaker = gsub("[[:punct:][:blank:]]+", "", speaker), # removes all punctuation
-    speaker = tolower(speaker),
-    speaker = recode(speaker, 'doctor' = 'dr','mom' = 'mother')
+getBiasTranscripts <- function(dirs) {
+  
+  transcript_df <- data.frame(
+    file_id = character(),
+    speaker = character(),
+    speech = character(),
+    text = character(),
+    text_ok = logical(),
+    text_pieces = integer(),
+    text_remainder = character(),
+    file_type = character()
   )
-table(transcript_df$speaker)
+  
+  for (dir in dirs) { 
+    fNames <- list.files(
+    path = dir, 
+    pattern = '*.docx')
+  
+    for (f in fNames) {
+      file_type <- if_else(is.na(stringr::str_match(f,"d_(.*?).docx")[[1,2]]), 'single_clinician',stringr::str_match(f,"d_(.*?).docx")[[1,2]])
+      t <- docxtractr::read_docx(here(dir,f))
+      tbl_no <- docxtractr::docx_tbl_count(t)
+      print(paste0(f,'; number of tables: ',tbl_no))
+      if (tbl_no == 0) {
+        df <- officer::read_docx(here(dir,f)) %>%
+          officer::docx_summary() %>%
+          select(text) %>%
+          separate_wider_delim(text, ":",
+                               names = c("speaker", "speech"),
+                               too_many = "merge",
+                               too_few = "debug")
+        df['file_id'] <- substr(f,1,5)
+        df$sequence <- seq(1:nrow(df))
+        df$file_type <- file_type
+        transcript_df <- rbind(transcript_df,df)
+      } else if (tbl_no == 1) {
+        df <- docxtractr::docx_extract_tbl(t, 1, header = FALSE) %>%
+          mutate(
+            V1 = stringr::str_remove(V1, pattern = " .*"),
+            V1 = tolower(V1),
+            file_id = substr(f,1,5)
+          ) %>%
+          rename('speaker' = V1,'speech' = V2)
+        df$sequence <- seq(1:nrow(df))
+        df$file_type <- file_type
+        transcript_df <- bind_rows(transcript_df,df)
+      } else {
+        print(paste('Too many tables for:',f))
+      }
+    }
+  }
+  transcript_df <- transcript_df %>%
+    janitor::clean_names() %>%
+    mutate(
+      speaker = gsub("[[:punct:][:blank:]]+", "", speaker), # removes all punctuation
+      speaker = tolower(speaker),
+      # speaker = recode(speaker, 'doctor' = 'dr','mom' = 'mother'),
+      study = 'bias'
+    )
+  return(transcript_df)
+}
 
 ######################
 ######################
 # for SDM transcripts:
 ######################
 ######################
-for (f in fNames) {
-  t <- docxtractr::read_docx(here(dir,f))
-  tbl_no <- docxtractr::docx_tbl_count(t)
-  print(paste0(f,'; number of tables: ',tbl_no))
-  if (tbl_no == 0) {
-    # print(paste("uh oh... No tables for:",f))
-    df <- officer::read_docx(here(dir,f)) %>%
-      officer::docx_summary() %>%
-      select(text) %>%
-      separate_wider_delim(text, ":",
-                           names = c("Speaker", "speech"),
-                           too_many = "merge",
-                           too_few = "debug")
-    df['file_id'] <- substr(f,1,5)
-    df$sequence <- seq(1:nrow(df))
-    transcript_df <- rbind(transcript_df,df)
-  } else if (tbl_no == 1) {
-    print(paste('Hmm... one table for:',f))
-  } else if (tbl_no == 2) {
-    df <- docxtractr::docx_extract_tbl(t, 2, header = FALSE) %>%
-      mutate(
-        V1 = stringr::str_remove(V1, pattern = " .*"),
-        V1 = tolower(V1),
-        file_id = substr(f,1,5)
-      ) %>%
-      rename('Speaker' = V1,'speech' = V2)
-    df$sequence <- seq(1:nrow(df))
-    transcript_df <- bind_rows(transcript_df,df)
-  } else {
-    print(paste('more than 2 tables for:',f))
+
+getSDMTranscripts <- function(dirs) {
+  
+  transcript_df <- data.frame(
+    file_id = character(),
+    speaker = character(),
+    speech = character(),
+    text = character(),
+    text_ok = logical(),
+    text_pieces = integer(),
+    text_remainder = character(),
+    file_type = character()
+  )
+  for (dir in dirs) {
+   
+    fNames <- list.files(
+      path = dir, 
+      pattern = '*.docx')
+    
+    
+    for (f in fNames) {
+      file_type <- if_else(is.na(stringr::str_match(f,"d_(.*?).docx")[[1,2]]), 'single_clinician',stringr::str_match(f,"d_(.*?).docx")[[1,2]])
+      t <- docxtractr::read_docx(here(dir,f))
+      tbl_no <- docxtractr::docx_tbl_count(t)
+      print(paste0(f,'; number of tables: ',tbl_no))
+      if (tbl_no == 0) {
+        # print(paste("uh oh... No tables for:",f))
+        df <- officer::read_docx(here(dir,f)) %>%
+          officer::docx_summary() %>%
+          select(text) %>%
+          separate_wider_delim(text, ":",
+                               names = c("speaker", "speech"),
+                               too_many = "merge",
+                               too_few = "debug")
+        df['file_id'] <- substr(f,1,5)
+        df$sequence <- seq(1:nrow(df))
+        df$file_type <- file_type
+        transcript_df <- rbind(transcript_df,df)
+      } else if (tbl_no == 1) {
+        print(paste('Hmm... one table for:',f))
+      } else if (tbl_no == 2) {
+        df <- docxtractr::docx_extract_tbl(t, 2, header = FALSE) %>%
+          mutate(
+            V1 = stringr::str_remove(V1, pattern = " .*"),
+            V1 = tolower(V1),
+            file_id = substr(f,1,5)
+          ) %>%
+          rename('speaker' = V1,'speech' = V2)
+        df$sequence <- seq(1:nrow(df))
+        df$file_type <- file_type
+        transcript_df <- bind_rows(transcript_df,df)
+      } else {
+        print(paste('more than 2 tables for:',f))
+        }
     }
+  } # end of directory loop 
+  transcript_df <- transcript_df %>%
+    janitor::clean_names() %>%
+    mutate(
+      speaker = gsub("[[:punct:][:blank:]]+", "", speaker), # removes all punctuation
+      speaker = tolower(speaker),
+      # speaker = recode(speaker, 'doctor' = 'dr', 'physician' = 'dr','mom' = 'mother'),
+      study = 'sdm'
+    )
+  return(transcript_df)
 }
 
-transcript_df <- transcript_df %>%
-  janitor::clean_names() %>%
-  mutate(
-    speaker = gsub("[[:punct:][:blank:]]+", "", speaker), # removes all punctuation
-    speaker = tolower(speaker),
-    speaker = recode(speaker, 'doctor' = 'dr', 'physician' = 'dr','mom' = 'mother')
-  )
-table(transcript_df$speaker)
+######################
+######################
+# for CONNECTS transcripts:
+######################
+######################
 
+getConnectsTranscripts <- function(dirs) {
+  
+  transcript_df <- data.frame(
+    file_id = character(),
+    speaker = character(),
+    speech = character(),
+    text = character(),
+    text_ok = logical(),
+    text_pieces = integer(),
+    text_remainder = character(),
+    file_type = character()
+  )
+
+  for (dir in dirs) {
+    
+    fNames <- list.files(
+      path = dir, 
+      pattern = '*.docx')
+    
+    for (f in fNames) {
+      file_type <- if_else(is.na(stringr::str_match(f,"d_(.*?).docx")[[1,2]]), 'single_clinician',stringr::str_match(f,"d_(.*?).docx")[[1,2]])
+      t <- docxtractr::read_docx(here(dir,f))
+      tbl_no <- docxtractr::docx_tbl_count(t)
+      print(paste0(f,'; number of tables: ',tbl_no)) 
+      
+      if (tbl_no == 2) {
+        df <- docxtractr::docx_extract_tbl(t, 2, header = FALSE) %>%
+          mutate(
+            V1 = stringr::str_remove(
+              stringr::str_remove(V1, pattern = 'ENROLLED *'), 
+              pattern = ":.*"),
+            # V1 = tolower(V1),
+            file_id = substr(f,1,5)
+          ) %>%
+          rename('speaker' = V1,'speech' = V2)
+        df$sequence <- seq(1:nrow(df))
+        
+        transcript_df <- bind_rows(transcript_df,df)
+      } else {
+        print(paste('SKIPPING... Something other than 2 tables for:',f))
+      }
+      
+      } # end of F loop
+  } # end of dirs loop
+  transcript_df <- transcript_df %>%
+    janitor::clean_names() %>%
+    mutate(
+      speaker = gsub("[[:punct:][:blank:]]+", "", speaker), # removes all punctuation
+      speaker = tolower(speaker),
+      speaker = recode(speaker, 'doctor' = 'dr', 'physician' = 'dr','mom' = 'mother'),
+      study = 'connects'
+    )
+  return(transcript_df)
+}
 
 # General function for reading transcripts from word docs
 
